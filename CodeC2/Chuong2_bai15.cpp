6796b4a25a16de76ce21c4f4bb7a7974f6ade9b1
Perry.hih <105875447+NguyenPhong0110@users.noreply.github.com>
2022-08-30 13:50:08 +0700

    Add files via upload

--- CodeC2/Chuong2_bai15.cpp
+++ CodeC2/Chuong2_bai15.cpp
@@ -1,143 +1,150 @@
 #include<iostream>
+#define max 100
 using namespace std;
-//=============================
-struct soHang {
-    int soMu;
-    float heSo;
-    struct soHang* next;
-};
-//=============================
-void nhapDuLieu(struct soHang* sh)
+int a[max], b[max];
+
+void Input(int a[], int& n)
 {
-    float temp;
-    struct soHang* tg;
-    tg = (struct soHang*)malloc(sizeof(struct soHang));
-
-    printf("\n Nhap vao so mu : "); scanf("%d", &tg->soMu);
-    printf("\n Nhap vao he so :");  scanf("%f", &temp);
-    tg->heSo = temp;
-
-    tg->next = NULL;
-
-    *sh = *tg;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
 }
-//=============================
-soHang* insertToHead(soHang* first, int soMu, float heSo) {
-    soHang* newNode = (soHang*)malloc(sizeof(soHang));
-
-    newNode->soMu = soMu;
-    newNode->heSo = heSo;
-
-    newNode->next = first;
-    first = newNode;
-
-    return first;
+void Output(int a[], int& n)
+{
+	for (int i = 0; i < n; i++)
+		cout << a[i] << " ";
 }
-//=============================
-soHang* insertToMiddle(soHang* current, int soMu, float heSo) {
-    soHang* newNode = (soHang*)malloc(sizeof(soHang));
-
-    newNode->soMu = soMu;
-    newNode->heSo = heSo;
-
-    newNode->next = current->next;
-    current->next = newNode;
-
-    return newNode;
+void Intersect(int a[], int b[], int& n, int& m)
+{
+	int c[max];
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+		{
+			if (a[i] == b[j])
+			{
+				c[i] = a[i];
+				cout << c[i] << " ";
+			}
+		}
+	cout << endl;
 }
-//=============================
-soHang* insertToLast(soHang* first, int soMu, float heSo) {
-    soHang* newNode = (soHang*)malloc(sizeof(soHang));
-    soHang* current = first;
-
-    newNode->soMu = soMu;
-    newNode->heSo = heSo;
-
-    // Duyet danh sach den cuoi danh sach
-    while (current != NULL) {
-        current = current->next;
-    }
-
-    current->next = newNode;
-
-    return first;
-
+int Check(int a[], int& n, int& x)
+{
+	for (int i = 0; i < n; i++)
+		if (a[i] == x)
+			return 1;
+	return 0;
 }
-//=============================
-void hienThi(soHang* first) {
-    soHang* tempNode = first;
-
-    while (tempNode != NULL) {
-        if (tempNode->heSo > 0)
-        {
-            printf(" +%4.0fX^%d", tempNode->heSo, tempNode->soMu);
-        }
-        else {
-            printf(" -%4.0fX^%d", tempNode->heSo, tempNode->soMu);
-        }
-
-        tempNode = tempNode->next;
-    }
-
+void Union(int a[], int b[], int& n, int& m)
+{
+	for (int i = 0; i < n; i++)
+		cout << a[i] << " ";
+	for (int i = 0; i < m; i++)
+	{
+		if (!Check(a, n, b[i]))
+			cout << b[i] << " ";
+	}
+	cout << endl;
 }
-//=============================
+void Compensation(int a[], int b[], int& n, int& m)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (Check(b, m, a[i]) == 0)
+			cout << a[i] << " ";
+	}
+	for (int i = 0; i < m; i++)
+	{
+		if (Check(a, n, b[i]) == 0)
+			cout << b[i] << " ";
+	}
+	cout << endl;
+}
+void menu(int& n, int& m)
+{
+	system("cls");
+	char choose;
+	cout << "=====BT 15 Chuong 2======\n";
+	cout << "1.Khoi tao 2 danh sach\n";
+	cout << "2.Xuat cac phan tu trong 1 danh sach\n";
+	cout << "3.Xuat cac phan tu thuoc tap hop cua 2 danh sach\n";
+	cout << "4.Xuat cac phan tu thuoc tap giao cua 2 danh sach\n";
+	cout << "5.Xuat cac phan tu thuoc phan bu cua 2 danh sach\n";
+	cout << "6.Thoat\n";
+	cout << "Nhap lua chon cua ban: ";
+	cin >> choose;
+	switch (choose)
+	{
+	case '1':
+	{
+		cout << "Nhap so phan tu cho danh sach 1: ";
+		cin >> n;
+		Input(a, n);
+		cout << "Nhap so phan tu cho danh sach 2: ";
+		cin >> m;
+		Input(b, m);
+		break;
+	}
+	case '2':
+	{
+		int x;
+		do
+		{
+			cout << "Nhap danh sach ma ban muon xuat: ";
+			cin >> x;
+		} while (x != 1 && x != 2);
+		if (x == 1)
+		{
+			cout << "Danh sach 1: ";
+			Output(a, n);
+			cout << endl;
+		}
+		else
+		{
+			cout << "Danh sach 2: ";
+			Output(b, m);
+			cout << endl;
+		}
+		break;
+	}
+	case'3':
+	{
+		cout << "Tap hop cac phan tu cua 2 danh sach gom: ";
+		Union(a, b, n, m);
+		break;
+	}
+	case'4':
+	{
+		cout << "Tap giao cua 2 danh sach la: ";
+		Intersect(a, b, n, m);
+		break;
+	}
+	case'5':
+	{
+		cout << "Phan bu cua 2 danh sach la: ";
+		Compensation(a, b, n, m);
+		break;
+	}
+	case'6':
+	{
+		cout << "Chuong Trinh Ket Thuc\n";
+		return;
+	}
+	default:
+	{
+		cout << "Nhap sai, nhap lai\n";
+		break;
+	}
+	}
+	system("pause");
+	return menu(n, m);
+}
 int main()
 {
-    int chon;
-    int soMu;
-    float heSo;
-    soHang* tempNode, * first = NULL;
-    struct soHang* current1 = NULL;
-    struct soHang* list = NULL, * dlist = NULL;
-    struct soHang* head = NULL;
-    system("cls");
-    cout << "===========BAI TAP 15 _ CHUONG 2==========" << endl;
-    cout << "14.1 Khai bao cau truc danh sach" << endl;
-    cout << "14.2 Nhap vao da thuc" << endl;
-    cout << "14.3 Xuat da thuc" << endl;
-    cout << "14.4 Cong hai da thuc" << endl;
-    cout << "14.5 Tru hai da thuc" << endl;
-    cout << "14.6 Nhan hai da thuc" << endl;
-    cout << "14.7 Chia hai da thuc" << endl;
-
-    cout << "0.Thoat" << endl;
-    do
-    {
-        cout << "\nMoi ban chon: ";
-        cin >> chon;
-        switch (chon)
-        {
-        case 1:
-            cout << "Khai bao cau truc danh sach thanh cong!" << endl;
-            break;
-        case 2:
-            cout << "\n Nhap vao He So :"; cin >> heSo;
-            cout << "\n Nhap vao so mu :"; cin >> soMu;
-
-            if (list == NULL)
-            {
-                first = inSertToHead(first, soMu, heSo);
-                break;
-            }
-
-            list = first;
-
-            insertToList();
-            break;
-            break;
-        case 3:
-            calculateValue();
-            break;
-        case 4:
-            tinhDaoHam();
-            break;
-        case 5:
-            hienThi();
-            break;
-        default: chon = 0;
-        }
-    } while (chon != 0);
-    system("pause");
-
-    return 0;
+	int n = 0, m = 0;
+	menu(n, m);
+	system("pause");
+	return 0;
 }
--- CodeC2/Chuong2_bai16.cpp
+++ CodeC2/Chuong2_bai16.cpp
@@ -1,98 +1,87 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-
-struct Node {
-	int info;
-	Node* link;
-};
-
-Node* front, * rear;
-
-
-void init() {
-	front = rear = NULL;
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define ll long long
+typedef struct Node {
+	int data;
+	struct Node* next;
+}*node;
+void Init(node& phead) {
+	phead = NULL;
 }
-
-
-int isEmpty() {
-	if (front == NULL)
-		return 1;
-	return 0;
+int Empty(node phead) {
+	return phead == NULL;
 }
-
-
-int Push(int x) {
-	Node* p = new Node;
-	p->info = x;
-	p->link = NULL;
-	if (rear == NULL)
-		front = rear = p;
-	else
-	{
-		rear->link = p;
-		rear = p;
-	}
-	return 1;
+node createNode(int x) {
+	node p = new Node;
+	p->data = x;
+	p->next = NULL;
+	return p;
 }
-
-
-int Pop(int& x) {
-
-	if (front != NULL)
-	{
-		Node* p = front;
-		x = p->info;
-		front = p->link;
-		if (front == NULL)
-			rear = NULL;
-		delete p;
-		return 1;
-	}
-	return 0;
-}
-
-
-int main() {
-
-	int choice, kt, x;
-	system("cls");
-	cout << "=== Bai tap 16 - Chuong 2 ===\n";
-	cout << "1. Khoi tao QUEUE rong\n";
-	cout << "2. Kiem tra QUEUE co rong hay khong?\n";
-	cout << "3. Them phan tu vao QUEUE\n";
-	cout << "4. Lay phan tu ra khoi QUEUE\n";
-	cout << "5. Thoat\n";
-
-	do {
-		cout << "Vui long chon so: ";
-		cin >> choice;
-
-		switch (choice)
-		{
-		case 1:
-			init();
-			cout << "\nKhoi tao thanh cong\n";
-			break;
-		case 2:
-			kt = isEmpty();
-			if (kt == 1) cout << "Rong\n";
-			else cout << "Khong rong\n";
-			break;
-		case 3:
-			cout << "Moi nhap X: ";
-			cin >> x;
-			Push(x);
-			break;
-		case 4:
-			Pop(x);
-			cout << "Phan tu X duoc lay ra: " << x << endl;
-			break;
-		default:
-			break;
+void iLast(node& phead, int x) {
+	node tmp = createNode(x), q;
+	if (Empty(phead)) phead = tmp;
+	else {
+		q = phead;
+		while (q->next != NULL) {
+			q = q->next;
 		}
-	} while (choice != 5);
-
-	system("pause");
+		q->next = tmp;
+	}
+}
+void input(node& phead) {
+	int n, x;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		iLast(phead, x);
+	}
+}
+void output(node phead) {
+	for (node p = phead; p != NULL; p = p->next) {
+		cout << p->data << " ";
+	}
+}
+void giaoHaiTapHop(node phead1, node phead2) {
+	for (node p = phead1; p != NULL; p = p->next) {
+		for (node q = phead2; q != NULL; q = q->next) {
+			if (p->data == q->data) {
+				cout << q->data << " ";
+			}
+		}
+	}
+}
+void phanTuBu(node phead1, node phead2) {
+	for (node p = phead1; p != NULL; p = p->next) {
+		for (node q = phead2; q != NULL; q = q->next) {
+			if (p->data == q->data) {
+				p->data = 0;
+			}
+		}
+	}
+	for (node p = phead1; p != NULL; p = p->next) {
+		if (p->data != 0) {
+			cout << p->data << " ";
+		}
+	}
+}
+int main() {
+	node phead1, phead2;
+	Init(phead1);
+	Init(phead2);
+	cout << "\nNhap so luong phan tu cua danh sach 1: ";
+	input(phead1);
+	cout << "\nNhap so luong phan tu cua danh sach 2: ";
+	input(phead2);
+	cout << "Danh sach 1: ";
+	output(phead1);
+	cout << "\n";
+	cout << "Danh sach 2: ";
+	output(phead2);
+	cout << "\n";
+	cout << "\nCac phan tu giao cua hai tap hop: ";
+	giaoHaiTapHop(phead1, phead2);
+	cout << "\nPhan tu bu cua hai tap hop: ";
+	phanTuBu(phead1, phead2);
 	return 0;
 }
